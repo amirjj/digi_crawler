968122e8974a725c27a02f0a92f59fdb981a19ac
amirjj <amir.jamshidijam@gmail.com>
2023-01-16 23:43:34 +0330

    config updated with new links

--- config.py
+++ config.py
@@ -19,7 +19,83 @@ SEARCH_TREE = {
     'v1': {
         'data': {
             'main_categories': {
-                'categories'
+                'categories': {
+                    'urls': [
+                            '/main/vehicles/',
+                            '/main/electronic-devices/',
+                            '/main/mobile/',
+                            '/main/apparel/',
+                            '/main/food-beverage/',
+                            '/main/rural-products/',
+                            '/main/mother-and-child/',
+                            '/main/personal-appliance/',
+                            '/main/home-and-kitchen/',
+                            '/main/book-and-media/',
+                            '/main/sport-entertainment/',
+                    ]
+                }
+            },
+            'best_selling_products': {
+                'url': '/best-selling/'
+            },
+            'digikala_magazine': {
+                'url': '/mag/'
+            },
+            'digiplus': {
+                'cash_back_products': {
+                    'url': '/search/?digiplus%5B0%5D=has_plus_cash_back'
+                },
+                'jet_delivery_products': {
+                    'url': '/search/?digiplus%5B0%5D=has_jet_delivery&sort=4'
+                }
+            },
+            'fresh_incredible_products': {
+                'url': '/fresh-offers/'
+            },
+            'incredible_products': {
+                'url': '/incredible-offers/'
+            },
+            'popular_brands': {
+                'urls': [
+                    '/brand-landing/pril/',
+                    '/brand-landing/persil/',
+                    '/brand-landing/king-star/',
+                    '/brand-landing/silicon-power/',
+                    '/brand/huawei/',
+                    '/brand-landing/pakshoma/',
+                    '/brand-landing/cheshme-noor/',
+                    '/brand/miscellaneous/',
+                    '/brand/xiaomi/',
+                    '/brand-landing/samsung/',
+                    '/brand-landing/kalleh/',
+                    '/brand/my/',
+                    '/brand/bioaqua/',
+                    '/brand-landing/active/',
+                    '/brand-landing/tsco/',
+                    '/brand-landing/zarmakaron/',
+                    '/brand-landing/leitu/',
+                    '/brand/shahr-glass/',
+                    '/brand/fresh-by-digikala/',
+                    '/brand/mr-rad/'
+                ]
+            },
+            'recommendation_sub_categories': {
+                'url': [
+                    '/search/category-mobile-phone/',
+                    '/search/category-cell-phone-pouch-cover/',
+                    '/search/category-headphone/',
+                    '/search/category-smart-watch/',
+                    '/search/category-notebook-netbook-ultrabook/',
+                    '/search/category-tablet/',
+                    '/search/category-mouse/',
+                    '/search/category-men-socks-tights/',
+                    '/search/category-station-gaming-consoles/',
+                    '/search/category-women-socks-/',
+                    '/search/category-speaker/',
+                    '/search/category-keyboard/',
+                    '/search/category-doll-house/',
+                    '/search/category-monitor/',
+                ]
             }
         }
     },
@@ -30,5 +106,4 @@ SEARCH_TREE = {
             }
         }
     },
-
 }
